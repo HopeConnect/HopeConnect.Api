d57f0c4485069f47605ffe32293be0450f9a96f2
Serkan Özbey Kurucu <serkan.kurucu@d-teknoloji.com.tr>
2024-01-18 11:04:06 +0300

    response service added

---
+++ HopeConnect.Customer.Api/Shared/Abstract/IServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿using HopeConnect.Customer.Api.Shared.ComplexTypes;
+
+namespace HopeConnect.Customer.Api.Shared.Abstract
+{
+	public interface IServiceResponse
+	{
+		ResponseCode ResponseCode { get;}
+		string Message { get; }
+	}
+}
