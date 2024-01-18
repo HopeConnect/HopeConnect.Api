f300e60f67aec7042b42f3f2bd8491a593fd60b4
Serkan Özbey Kurucu <serkan.kurucu@d-teknoloji.com.tr>
2024-01-18 13:58:31 +0300

    user controller added

---
+++ HopeConnect.Customer.Api/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using HopeConnect.Customer.Api.BusinessUnit;
+using HopeConnect.Customer.Api.Infrastructure.Dto;
+using HopeConnect.Customer.Api.Shared.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HopeConnect.Customer.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[Authorize]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserBusinessUnit _userBusinessUnit;
+		public UserController(IUserBusinessUnit userBusinessUnit)
+		{
+			_userBusinessUnit = userBusinessUnit;
+		}
+		[HttpGet]
+		[Route("GetUserByUserFirebaseId")]
+		public async Task<Response<IList<UserListDto>>> GetUserByUserFirebaseIdAsync()
+		{
+			return await _userBusinessUnit.TGetUserByUserFirebaseIdAsync();
+		}
+	}
+}
